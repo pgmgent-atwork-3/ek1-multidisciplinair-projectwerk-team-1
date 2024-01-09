61ffb09241c8a814ed76cc7374e5eadd8db54da7
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2024-01-09 16:02:14 +0100

    removed unused imports

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -19,24 +19,42 @@ const Admin = async () => {
 
   return (
     <>
-      <h2>Aanpasbare waarden</h2>
-      <div className="flex justify-center gap-4 items-center mt-16">
-      <a href="/admin/rings" className="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded">
-        Ringen
-      </a>
-      <a href="/admin/year-colors" className="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded">
-        Jaar kleuren en post prijzen
-      </a>
-    </div>
-    <h2>Verkoop</h2>
-      <div className="flex justify-center gap-4 items-center mt-16">
-      <a href="/orderfinalizing" className="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded">
-        Maand verkoop
-      </a>
-      <a href="/orderoverzicht" className="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded">
-        Order geschiedenis
-      </a>
-    </div>
+      <div className="grid grid-cols-2 py-4">
+        <div className="flex ml-6 items-center">
+          <h2 className="text-2xl mr-2">Verkoop:</h2>
+          <div className="flex justify-around gap-4">
+            <a
+              href="/orderfinalizing"
+              className="bg-blue-700 hover:bg-blue-800 text-white text-center font-bold py-2 px-4 rounded"
+            >
+              Maand verkoop
+            </a>
+            <a
+              href="/orderoverzicht"
+              className="bg-blue-700 hover:bg-blue-800 text-white text-center font-bold py-2 px-4 rounded"
+            >
+              Order geschiedenis
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-end mr-6 items-center">
+          <h2 className="text-2xl mr-2">Aanpasbare waarden:</h2>
+          <div className="flex justify-around gap-4">
+            <a
+              href="/admin/rings"
+              className="bg-slate-500 hover:bg-slate-600 text-white text-center font-bold py-2 px-4 rounded"
+            >
+              Ringen
+            </a>
+            <a
+              href="/admin/year-colors"
+              className="bg-slate-500 hover:bg-slate-600 text-white text-center font-bold py-2 px-4 rounded"
+            >
+              Jaar kleuren en post prijzen
+            </a>
+          </div>
+        </div>
+      </div>
       <h2 className="text-4xl mt-4 ml-4">Users</h2>
       <div>
         <div className="p-4 shadow-md rounded-lg bg-blue-100 mt-5 grid grid-cols-7 gap-4 ml-6 mr-6">
