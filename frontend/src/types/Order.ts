b25078d5706264c9bc55d946b7ef5220d3be8448
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2024-01-23 14:57:02 +0100

    typescript fix

--- frontend/src/types/Order.ts
+++ frontend/src/types/Order.ts
@@ -25,5 +25,20 @@ type Order = {
 }
 
 type OrderQuery = {
-  
+  id: string;
+  color_ring: {
+    size: number;
+    price: number;
+    amount: number;
+  }[];
+  inox_ring: {
+    size: number;
+    price: number;
+    amount: number;
+  }[];
+  month: number;
+  payment: string;
+  user: string;
+  totaal: number;
+  year: number;
 }
