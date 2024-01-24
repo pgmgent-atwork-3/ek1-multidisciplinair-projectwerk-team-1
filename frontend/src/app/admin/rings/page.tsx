6aee6ae9379eb19b06d1a6b56e166a5b0ca8ac93
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2024-01-24 17:24:26 +0100

    styling

--- frontend/src/app/admin/rings/page.tsx
+++ frontend/src/app/admin/rings/page.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { loadColorRings, loadInoxRings, saveColorRings, saveInoxRings } from "@/app/api/api";
+import {
+  loadColorRings,
+  loadInoxRings,
+  saveColorRings,
+  saveInoxRings,
+} from "@/app/api/api";
 import { useEffect, useState } from "react";
 
 const Rings = () => {
@@ -24,60 +29,70 @@ const Rings = () => {
     fetchInoxRings();
   }, []);
 
-  const handleChangeColor = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeColor = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     if (!event || !event.target || !event.target.value) {
       return;
     }
     const { value, name } = event.target;
     const newSize = parseFloat(value);
-  
+
     const updatedColorRings = [...colorRings];
-    const existingRing = updatedColorRings.find((ring, i) => i !== index && ring.size === newSize);
-  
+    const existingRing = updatedColorRings.find(
+      (ring, i) => i !== index && ring.size === newSize
+    );
+
     if (existingRing) {
       // Handle the case where the size already exists (e.g., show an error message)
-      console.error('Size already exists');
+      console.error("Size already exists");
       return;
     }
-  
+
     const updatedRing = { ...updatedColorRings[index] };
-  
+
     if (name === "size") {
       updatedRing.size = newSize;
     }
     if (name === "price") {
       updatedRing.price = parseFloat(value);
     }
-  
+
     updatedColorRings[index] = updatedRing;
     setColorRings(updatedColorRings);
   };
-  
-  const handleChangeInox = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleChangeInox = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     if (!event || !event.target || !event.target.value) {
       return;
     }
     const { value, name } = event.target;
     const newSize = parseFloat(value);
-  
+
     const updatedInoxRings = [...inoxRings];
-    const existingRing = updatedInoxRings.find((ring, i) => i !== index && ring.size === newSize);
-  
+    const existingRing = updatedInoxRings.find(
+      (ring, i) => i !== index && ring.size === newSize
+    );
+
     if (existingRing) {
       // Handle the case where the size already exists (e.g., show an error message)
-      console.error('Size already exists');
+      console.error("Size already exists");
       return;
     }
-  
+
     const updatedRing = { ...updatedInoxRings[index] };
-  
+
     if (name === "size") {
       updatedRing.size = newSize;
     }
     if (name === "price") {
       updatedRing.price = parseFloat(value);
     }
-  
+
     updatedInoxRings[index] = updatedRing;
     setInoxRings(updatedInoxRings);
   };
@@ -86,23 +101,27 @@ const Rings = () => {
     const updatedColorRings = [...colorRings];
     updatedColorRings.push({ size: 0, price: 0, amount: 0 });
     setColorRings(updatedColorRings);
-  }
+  };
 
   const addInoxRing = () => {
     const updatedInoxRings = [...inoxRings];
     updatedInoxRings.push({ size: 0, price: 0, amount: 0 });
     setInoxRings(updatedInoxRings);
-  }
+  };
 
   const handleDeleteColor = (index: number) => {
-    const updatedColorRings = colorRings.slice(0, index).concat(colorRings.slice(index + 1));
+    const updatedColorRings = colorRings
+      .slice(0, index)
+      .concat(colorRings.slice(index + 1));
     setColorRings(updatedColorRings);
-  }
+  };
 
   const handleDeleteInox = (index: number) => {
-    const updatedInoxRings = inoxRings.slice(0, index).concat(inoxRings.slice(index + 1));
+    const updatedInoxRings = inoxRings
+      .slice(0, index)
+      .concat(inoxRings.slice(index + 1));
     setInoxRings(updatedInoxRings);
-  }
+  };
 
   const handleSubmitColor = async () => {
     await saveColorRings(colorRings);
@@ -113,93 +132,149 @@ const Rings = () => {
   };
 
   return (
-    <>
-    <h2 className="text-4xl mt-4 ml-4">Ring prijzen en maten</h2>
-    <div className="grid grid-cols-2 mx-auto">
-      <div className="w-96">
-        <h2 className="text-2xl mt-4 ml-4">Gekleurde Ringen</h2>
-        <ul className="flex justify between flex-col">
-          {colorRings.map((ring, index) => (
-            <li key={ring.size} className="flex items-center justify-between py-2 px-4">
-              <label htmlFor="size" className="text-lg font-bold pr-2 pl-8">Maat</label>
-              <input
-                type="number"
-                className="border-2 border-gray-400 rounded-md px-2 py-1"
-                name="size"
-                step={0.1}
-                value={ring.size}
-                onChange={(event) => handleChangeColor(index, event)}
-                disabled={!editingColor}
-              />
-              <label htmlFor="price" className="text-lg font-bold pr-2 pl-8">Prijs</label>
-              <input
-                type="number"
-                className="border-2 border-gray-400 rounded-md px-2 py-1"
-                name="price"
-                step={0.01}
-                value={ring.price}
-                onChange={(event) => handleChangeColor(index, event)}
-                disabled={!editingColor}
-              />
-              {editingColor ? (
-                <button className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => handleDeleteColor(index)}>Verwijderen</button>
-              ) : null}
+    <div className="container lg m-auto mt-6 mb-20">
+      <h1 className="text-4xl font-bold flex justify-center">
+        Ring prijzen en maten
+      </h1>
+      <div className="flex">
+        <div className="w-1/2">
+          <h2 className="text-2xl font-semibold mt-4 ml-4">Gekleurde Ringen</h2>
+          <ul className="flex items-start flex-col">
+            <li className="grid grid-cols-3 gap-32 py-2 px-4">
+              <h3 className="text-lg font-bold pr-2 pl-8">Maat</h3>
+              <h3 className="text-lg font-bold pr-2 pl-8">Prijs</h3>
             </li>
-          ))}
-        </ul>
-        {editingColor ? (
-          <div className="flex space-x-4 mt-4">
-            <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => setEditingColor(false)}>Annuleren</button>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={handleSubmitColor}>Bevestigen</button>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={addColorRing}>Ring toevoegen</button>
-          </div>
-          ) : (
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={() => setEditingColor(true)}>Bewerken</button>
-        )}
-      </div>
-      <div className="w-96">
-        <h2 className="text-2xl mt-4 ml-4">Inox ringen</h2>
-        <ul className="flex justify between flex-col">
-          {inoxRings.map((ring, index) => (
-            <li key={ring.size} className="flex items-center justify-between py-2 px-4">
-              <label htmlFor="size" className="text-lg font-bold pr-2 pl-8">Maat</label>
-              <input
-                type="number"
-                className="border-2 border-gray-400 rounded-md px-2 py-1"
-                name="size"
-                step={0.1}
-                value={ring.size}
-                onChange={(event) => handleChangeInox(index, event)}
-                disabled={!editingInox}
-              />
-              <label htmlFor="price" className="text-lg font-bold pr-2 pl-8">Prijs</label>
-              <input
-                type="number"
-                className="border-2 border-gray-400 rounded-md px-2 py-1"
-                name="price"
-                step={0.01}
-                value={ring.price}
-                onChange={(event) => handleChangeInox(index, event)}
-                disabled={!editingInox}
-              />
-              {editingInox ? (
-                <button className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => handleDeleteInox(index)}>Verwijderen</button>
-              ) : null}
-            </li>
-          ))}
-        </ul>
-        {editingInox ? (
+            {colorRings.map((ring, index) => (
+              <li key={ring.size} className="grid grid-cols-3 gap-4 py-2 px-4">
+                <input
+                  type="number"
+                  className="border-2 border-gray-400 rounded-md px-2 py-1"
+                  name="size"
+                  step={0.1}
+                  value={ring.size}
+                  onChange={(event) => handleChangeColor(index, event)}
+                  disabled={!editingColor}
+                />
+                <input
+                  type="number"
+                  className="border-2 border-gray-400 rounded-md px-2 py-1"
+                  name="price"
+                  step={0.01}
+                  value={ring.price}
+                  onChange={(event) => handleChangeColor(index, event)}
+                  disabled={!editingColor}
+                />
+                {editingColor ? (
+                  <button
+                    className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                    onClick={() => handleDeleteColor(index)}
+                  >
+                    Verwijderen
+                  </button>
+                ) : null}
+              </li>
+            ))}
+          </ul>
+          {editingColor ? (
             <div className="flex space-x-4 mt-4">
-              <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => setEditingInox(false)}>Annuleren</button>
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={handleSubmitInox}>Bevestigen</button>
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={addInoxRing}>Ring toevoegen</button>
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                onClick={() => setEditingColor(false)}
+              >
+                Annuleren
+              </button>
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                onClick={handleSubmitColor}
+              >
+                Bevestigen
+              </button>
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                onClick={addColorRing}
+              >
+                Ring toevoegen
+              </button>
             </div>
           ) : (
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={() => setEditingInox(true)}>Bewerken</button>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              onClick={() => setEditingColor(true)}
+            >
+              Bewerken
+            </button>
           )}
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-2xl font-semibold mt-4 ml-4">Inox ringen</h2>
+          <ul className="flex items-start flex-col">
+            <li className="grid grid-cols-3 gap-32 py-2 px-4">
+              <h3 className="text-lg font-bold pr-2 pl-8">Maat</h3>
+              <h3 className="text-lg font-bold pr-2 pl-8">Prijs</h3>
+            </li>
+            {inoxRings.map((ring, index) => (
+              <li key={ring.size} className="grid grid-cols-3 gap-4 py-2 px-4">
+                <input
+                  type="number"
+                  className="border-2 border-gray-400 rounded-md px-2 py-1"
+                  name="size"
+                  step={0.1}
+                  value={ring.size}
+                  onChange={(event) => handleChangeInox(index, event)}
+                  disabled={!editingInox}
+                />
+                <input
+                  type="number"
+                  className="border-2 border-gray-400 rounded-md px-2 py-1"
+                  name="price"
+                  step={0.01}
+                  value={ring.price}
+                  onChange={(event) => handleChangeInox(index, event)}
+                  disabled={!editingInox}
+                />
+                {editingInox ? (
+                  <button
+                    className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                    onClick={() => handleDeleteInox(index)}
+                  >
+                    Verwijderen
+                  </button>
+                ) : null}
+              </li>
+            ))}
+          </ul>
+          {editingInox ? (
+            <div className="flex space-x-4 mt-4">
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                onClick={() => setEditingInox(false)}
+              >
+                Annuleren
+              </button>
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                onClick={handleSubmitInox}
+              >
+                Bevestigen
+              </button>
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                onClick={addInoxRing}
+              >
+                Ring toevoegen
+              </button>
+            </div>
+          ) : (
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              onClick={() => setEditingInox(true)}
+            >
+              Bewerken
+            </button>
+          )}
+        </div>
       </div>
     </div>
-    </>
   );
 };
 
--- frontend/src/app/admin/year-colors/page.tsx
+++ frontend/src/app/admin/year-colors/page.tsx
@@ -4,7 +4,7 @@ import YearColors from "./year-colors";
 
 const Page = () => {
   return (
-    <div className="grid grid-cols-2 my-4 mx-10">
+    <div className="container lg grid grid-cols-2 my-6 m-auto">
       <div className="flex justify-center">
         <YearColors />
       </div>
